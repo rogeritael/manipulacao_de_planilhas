83d0b8f2d4421ac10ea91932320009fe58fdd5fd
Roger Rosa <rogeritael.contato@gmail.com>
2023-10-19 18:04:47 -0300

    refatorando o processamento de arquivos

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from classes.LeitorDeAcoes import LeitorDeAcoes
+
 from openpyxl import Workbook
 from openpyxl.chart import LineChart, Reference
 from openpyxl.styles import Font, PatternFill, Alignment
@@ -6,47 +8,43 @@ from datetime import date
 # acao = input('Qual codigo da açao voce quer processar?').upper()
 acao = 'bidi4'
 
-with open(f'./dados/{acao}.txt', 'r') as arquivo_cotacao:
-    # Pega todas as linhas do arquivo
-    linhas = arquivo_cotacao.readlines()
-    
-    # formata linha a linha da forma que queremos
-    linhas = [linha.replace('\n', '').split(';') for linha in linhas]
+leitor_de_acoes = LeitorDeAcoes(caminho_do_arquivo='./dados/')
+leitor_de_acoes.processaArquivo(acao)
 
-    #cria um planilha em memoria
-    workbook = Workbook()
+#cria um planilha em memoria
+workbook = Workbook()
 
-    # pega a atual ativa e muda o nome
-    aba_atual = workbook.active
-    aba_atual.title = "Dados"
+# pega a atual ativa e muda o nome
+aba_atual = workbook.active
+aba_atual.title = "Dados"
 
-    # criaçao do cabeçalho
-    aba_atual.append(['DATA', 'COTAÇAO', 'BANDA INFERIOR', 'BANDA SUPERIOR'])
+# criaçao do cabeçalho
+aba_atual.append(['DATA', 'COTAÇAO', 'BANDA INFERIOR', 'BANDA SUPERIOR'])
 
-    indice = 2
-    for linha in linhas:
-        # DATA
-        ano_mes_dia = linha[0].split(' ')[0].split('-')
-        data = date(
-            int(ano_mes_dia[0]),
-            int(ano_mes_dia[1]),
-            int(ano_mes_dia[2])
-        )
+indice = 2
+for linha in leitor_de_acoes.dados:
+    # DATA
+    ano_mes_dia = linha[0].split(' ')[0].split('-')
+    data = date(
+        int(ano_mes_dia[0]),
+        int(ano_mes_dia[1]),
+        int(ano_mes_dia[2])
+    )
 
-        #COTACAO
-        cotacao = float(linha[1])
+    #COTACAO
+    cotacao = float(linha[1])
 
 
-        #Preenchendo as colunas dinamicamente
-        aba_atual[f'A{indice}'] = data
-        aba_atual[f'B{indice}'] = cotacao
+    #Preenchendo as colunas dinamicamente
+    aba_atual[f'A{indice}'] = data
+    aba_atual[f'B{indice}'] = cotacao
 
-        #BANDA INFERIOR
-        aba_atual[f'C{indice}'] = f'=AVERAGE(B{indice}:B{indice+19}) - 2*STDEV(B{indice}:B{indice+19})'
-        #BANDA SUPERIOR
-        aba_atual[f'D{indice}'] = f'=AVERAGE(B{indice}:B{indice+19}) + 2*STDEV(B{indice}:B{indice+19}'
+    #BANDA INFERIOR
+    aba_atual[f'C{indice}'] = f'=AVERAGE(B{indice}:B{indice+19}) - 2*STDEV(B{indice}:B{indice+19})'
+    #BANDA SUPERIOR
+    aba_atual[f'D{indice}'] = f'=AVERAGE(B{indice}:B{indice+19}) + 2*STDEV(B{indice}:B{indice+19}'
 
-        indice += 1
+    indice += 1
 
 
     # NOVA ABA
