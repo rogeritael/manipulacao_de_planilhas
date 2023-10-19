5e3d2769c67f6788a91b2a2ceb18459606cb1b10
Roger Rosa <rogeritael.contato@gmail.com>
2023-10-18 21:00:11 -0300

    adicionando grafico a planilha

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from openpyxl import Workbook
+from openpyxl.chart import LineChart, Reference
+from openpyxl.styles import Font, PatternFill, Alignment
 from datetime import date
 
 # acao = input('Qual codigo da açao voce quer processar?').upper()
@@ -47,10 +49,37 @@ with open(f'./dados/{acao}.txt', 'r') as arquivo_cotacao:
         indice += 1
 
 
+    # NOVA ABA
+    aba_grafico = workbook.create_sheet('Grafico')
+    workbook.active = aba_grafico
+
+    #mesclagem de celulas
+    aba_grafico.merge_cells("A1:T2")
+
+    # CRIAÇAO DO CABECALHO
+    cabecalho = aba_grafico['A1'] #com a mesclagem toda essa a1 a t2 se tornaram a1
+    cabecalho.font = Font(b=True, sz=18, color='FFFFFF')
+    cabecalho.fill = PatternFill('solid', fgColor='333333')
+    cabecalho.alignment =  Alignment(vertical='center', horizontal='center')
+    cabecalho.value = f'Cotaçoes {acao}'
 
 
+    # CRIAÇAO DO GRAFICO
+    grafico = LineChart()
+    grafico.width = 33.87
+    grafico.height = 14.82
+    grafico.title = 'Historico de Cotaçoes'
+    grafico.x_axis.title = 'Data da Cotaçao'
+    grafico.y_axis.title = 'Valor da Cotaçao'
 
 
+    # Pegar dados de outra aba
+    cotacoes_referencia = Reference(aba_atual, min_col=2, min_row=2, max_col=4, max_row=indice)
+    datas_referencia = Reference(aba_atual, min_col=1, min_row=2, max_col=1, max_row=indice)
 
+    grafico.add_data(cotacoes_referencia)
+    grafico.set_categories(datas_referencia)
+
+    aba_grafico.add_chart(grafico, 'A3')
 
     workbook.save('./planilha/dados.xlsx')
